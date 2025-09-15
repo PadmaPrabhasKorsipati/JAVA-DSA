253221bf845f6ce261c32d10097665f8ebf84b30
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-09-15 16:52:06 +0530

    auto-commit

--- practice.java
+++ practice.java
@@ -1,8 +1,31 @@
+
+
+import java.util.Scanner;
+
+//swap numbers
 public class practice {
 
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter value of a:");
+        int a=sc.nextInt();
+        System.out.print("Enter value of b:");
+        int b=sc.nextInt();
+        System.out.println("Before swap:"+" a="+a +" b="+b);
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+
+        System.out.println("After swap:"+" a="+a +" b="+b);
         
-    }
+
+    sc.close();
+
+     }
+
+    
+
     
     
 }

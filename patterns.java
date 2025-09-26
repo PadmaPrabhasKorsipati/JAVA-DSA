9de8c391a44475d5db8f8f73153d2ba4d8e2e6c7
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-09-26 18:27:11 +0530

    auto-commit

--- patterns.java
+++ patterns.java
@@ -50,5 +50,28 @@ public class patterns {
 }
  */
 
+//character pattern 
 
- 
+import java.util.Scanner;
+
+public class patterns {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no of rows:");
+        int n=sc.nextInt();
+        int num=65;
+        int org=num;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((char)num +" ");
+                num+=1;
+            }
+            num=org;
+            System.out.println();
+
+        }
+        sc.close();
+    }
+}
+

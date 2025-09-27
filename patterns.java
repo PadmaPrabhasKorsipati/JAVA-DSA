22493d1cd7067c18621ac80dd44d606159afbf2b
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-09-27 13:44:49 +0530

    auto-commit

--- patterns.java
+++ patterns.java
@@ -24,7 +24,7 @@ public class patterns {
 }
 */
 
-
+ 
 //Half pyramid pattern
 /*
 import java.util.Scanner;
@@ -50,6 +50,8 @@ public class patterns {
 }
  */
 
+/* 
+
 //character pattern 
 
 import java.util.Scanner;
@@ -74,4 +76,34 @@ public class patterns {
         sc.close();
     }
 }
+*/
 
+//square hollow pattern
+
+import java.util.Scanner;
+
+public class patterns {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no.of rows:");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1 || j==1 || i==n || j==n){
+                    System.out.print("*");
+                }
+
+                else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println("");
+        }
+        sc.close();
+    }
+
+    
+}

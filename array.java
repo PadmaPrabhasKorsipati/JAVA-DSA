bb74facdebe13897687e9b68d739be9b806b076f
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-09-29 19:10:55 +0530

    auto-commit

--- array.java
+++ array.java
@@ -1,3 +1,10 @@
 public class array {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,8};
+        
+        for(int i=0;i<8;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
     
 }

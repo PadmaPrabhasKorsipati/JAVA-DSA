fdcaef283eecd5f25d531961389aa612425a667f
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-09-29 13:26:10 +0530

    auto-commit

--- patterns.java
+++ patterns.java
@@ -252,7 +252,7 @@ public class patterns {
 }
 */
 
-
+/* 
 //palindrome Triangle
 
 import java.util.Scanner;
@@ -287,4 +287,32 @@ public class patterns {
     
 }
 
+}
+
+*/
+
+
+import java.util.Scanner;
+
+public class patterns {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no.of rows:");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+
+            }
+            for(int k=1;k<=n;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+
+    }
 }

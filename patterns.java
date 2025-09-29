b3cb6616f3b15ac1f44bbe313407acad13e4ec6f
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-09-29 12:43:18 +0530

    auto-commit

--- patterns.java
+++ patterns.java
@@ -199,7 +199,7 @@ public class patterns {
 
 
 //number changing pattern
-
+/* 
 import java.util.Scanner;
 
 public class patterns {
@@ -219,5 +219,34 @@ public class patterns {
 
         sc.close();
         
+    }
+}
+*/
+
+//binary triangle 
+
+import java.util.Scanner;
+
+public class patterns {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no.of rows:");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print(1+" ");
+                }
+                else{
+                    System.out.print(0+" ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+
     }
 }

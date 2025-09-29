e982f9338a3805b2b0840b0eac53a828367c471c
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-09-29 22:29:47 +0530

    auto-commit

--- array.java
+++ array.java
@@ -1,3 +1,4 @@
+/* 
 import java.util.Scanner;
 
 public class array {
@@ -23,3 +24,53 @@ public class array {
 
     
 }
+*/
+
+
+
+//Linear Search 
+
+import java.util.Scanner;
+
+public class array {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no.of elements:");
+        int n=sc.nextInt();
+         System.out.print("Enter element to search:");
+         int search=sc.nextInt();
+
+         int sum=0;
+         int pos=0;
+
+         int[] arr=new int[n];
+
+         for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+
+         }
+
+         for(int j=0;j<n;j++){
+            if(arr[j]==search){
+              pos=j;
+              break;
+
+            }
+
+          
+
+         }
+
+         
+            if(pos!=-1){  System.out.println(search+" is found at index "+pos);}
+            
+            else{System.out.println(search+" is not found.");}
+
+
+
+         sc.close();
+        
+    }
+}
+

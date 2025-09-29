2fe04cc852a52caa7cb85580e97fcf8385c3ee19
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-09-29 19:24:10 +0530

    auto-commit

--- array.java
+++ array.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class array {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5,6,7,8};
-        
-        for(int i=0;i<8;i++){
-            System.out.print(arr[i]+" ");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no.of rows:");
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            
         }
+
+         System.out.println("You entered:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
     }
+    
+
     
 }

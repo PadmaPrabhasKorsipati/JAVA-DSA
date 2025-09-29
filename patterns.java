a3e72ce62ad9a606709460c72b594265b416d445
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-09-29 10:03:00 +0530

    auto-commit

--- patterns.java
+++ patterns.java
@@ -112,10 +112,7 @@ public class patterns {
 
 */
 
-
-
-
-
+/* 
 
 //Number Triangle 
 
@@ -140,5 +137,32 @@ public class patterns {
 
         sc.close();
 
+    }
+}
+
+*/
+
+
+
+
+
+//Number increasing pyramid pattern
+
+import java.util.Scanner;
+
+public class patterns {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no.of rows:");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }

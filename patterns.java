70242f232a0ceae9de2997c213364e282d19b90b
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-09-29 09:56:35 +0530

    auto-commit

--- patterns.java
+++ patterns.java
@@ -128,11 +128,11 @@ public class patterns {
         System.out.print("Enter no.of rows:");
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=n-i;j>=1;j++){
+            for(int j=1;j<=n-i;j++){
                 System.out.print(" ");
             }
-            for(int k=i;k<=n;k++){
-                System.out.print(i);
+            for(int k=1;k<=i;k++){
+                System.out.print(i+" ");
 
             }
             System.out.println();

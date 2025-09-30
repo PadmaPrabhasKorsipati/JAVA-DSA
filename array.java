7fc7f01ef69aae69d28d0134a47d343388047095
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-09-30 12:27:48 +0530

    auto-commit

--- array.java
+++ array.java
@@ -182,3 +182,27 @@ public class array {
 
 
 */
+
+//Reverse  an array 
+
+import java.util.Scanner;
+
+public class array {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no.of Elements:");
+        int n=sc.nextInt();
+        int[] arr= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+
+        }
+
+        System.out.println("Reversed array:");
+        for(int i=n-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}

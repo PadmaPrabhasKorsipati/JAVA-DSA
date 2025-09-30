2297ac1a8f8dd4318fff9a8d0ecab3f234b3d740
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-09-30 23:38:24 +0530

    auto-commit

--- sorting.java
+++ sorting.java
@@ -1,7 +1,10 @@
 
+/* 
+
 //Bubble Sort
 //Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order. 
-
+//Time Complexity: O(n2)
+Auxiliary Space: O(1)
 import java.util.Scanner;
 
 public class sorting {
@@ -41,3 +44,55 @@ sc.close();
     }
     
 }
+
+*/
+
+
+/* 
+//Selection sort
+//The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from the unsorted part and putting it at the beginning.
+//Time Complexity:  O(n2)
+//Auxiliary Space: O(1)
+
+import java.util.Scanner;
+
+public class sorting {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no.of Elements:");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+
+        }
+
+        for(int i=0;i<n;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+
+                }
+
+                int temp=arr[i];
+                arr[i]=arr[min];
+                arr[min]=temp;
+
+            }
+        }
+
+        System.out.println("Sorted Array:");
+
+for(int i=0;i<n;i++){
+    System.out.print(arr[i]+" ");
+}
+
+sc.close();
+
+
+    }
+}
+
+*/

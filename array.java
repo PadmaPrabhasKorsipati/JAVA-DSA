fe72e7e00a7c59a37ea055214a3d4dadcb86ba33
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-09-30 09:49:28 +0530

    auto-commit

--- array.java
+++ array.java
@@ -121,6 +121,10 @@ public class array {
 
         System.out.print("Enter no of Elements:");
         int n=sc.nextInt();
+
+        System.out.print("Enter Search elemnt:");
+        int target=sc.nextInt();
+
         int[] arr=new int[n];
         int sort;
 
@@ -128,25 +132,51 @@ public class array {
             arr[i]=sc.nextInt();
             
         }
-
+         for (int i = 0; i < n - 1; i++) {
         for(int j=0;j<n-1;j++){
             if(arr[j]>arr[j+1]){
                 sort=arr[j+1];
                 arr[j+1]=arr[j];
                 arr[j]=sort;
 
-
             }
+   }
+}
 
 
-             }
+
+   int low=0;
+   int high=n-1;
+   int found=0;
+
+   while(low<=high){
+    int mid=low +(high-low)/2;
+    
+    if(arr[mid]==target){
+        System.out.println("Element is found at index:"+mid);
+        found+=1;
+        break;
+    }
+    else if(arr[mid]<target){
+        low=mid+1;
 
 
-             for(int k=0;k<n;k++){
-                System.out.print(arr[k]+" ");
-            
-            
-        }
+    }
+
+    else{
+        high=mid-1;
+
+    }
+
+
+   }
+   
+    if(found==0){
+        System.out.println(" The given Element is not found in the given array ");
+    }
+
+
+
 
 
 

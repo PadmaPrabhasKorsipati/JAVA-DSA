8d02d57118cf21ebdbc3db6ea798650e8a4e9b8b
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-10-01 09:46:45 +0530

    auto-commit

--- sorting.java
+++ sorting.java
@@ -104,6 +104,14 @@ sc.close();
 public class sorting {
 
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no.of Elements:");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+
+        }
         
     }
 }

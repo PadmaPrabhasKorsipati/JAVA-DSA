1955ea1f0417b40929a2975e913744203c6c0ec8
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-10-03 14:41:03 +0530

    auto-commit

--- sorting.java
+++ sorting.java
@@ -119,7 +119,7 @@ public class sorting {
         }
 
         for(int i=1;i<n;i++){
-            for(int j=i-1;i>0;i--){
+            for(int j=i-1;j>=0;j--){
                 if(arr[j]>arr[i]){
                      temp=arr[j];
                      arr[j]=arr[i];

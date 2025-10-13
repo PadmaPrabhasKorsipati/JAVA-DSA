ef735a330dae16b168fd3e59b6ef486aa07a1d5b
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-10-13 14:10:24 +0000

    Oct 13, 2025, 7:40 PM

--- twodarray.java
+++ twodarray.java
@@ -49,16 +49,67 @@ public class twodarray {
         System.out.print("Enter no.of Columns:");
         int columns=sc.nextInt();
 
-        int[][] arr=new int[rows][columns];
+        int[][] matrix=new int[rows][columns];
 
         for(int i=0;i<rows;i++){
             for(int j=0;j<columns;j++){
-                arr[i][j]=sc.nextInt();
+                matrix[i][j]=sc.nextInt();
             }
         }
 
+        System.out.println("Spiral form");
+        printSpiral(matrix, rows, columns);
 
-        for(int )
+        }
 
-    }
+
+
+static void printSpiral(int[][] arr, int rows, int cols) {
+        int top=0; int right=columns-1;
+        int bottom=rows-1; int left=0;
+        
+
+        while(top<=bottom && right<=left){
+
+            //print left to right
+
+            for(int i=left;i<=right;i++){
+                System.out.print(arr[top][i] + " ");
+            }
+            top ++;
+
+            //print top to bottom
+
+            for(int i=top;i<=bottom;i++){
+                System.out.println(arr[i][right]+" ");
+
+            }
+            right--;
+
+            //print right to left
+
+            for(int i=right;i>=left;i--){
+                System.out.print(arr[bottom][i]+" ");
+            }
+            bottom--;
+
+            //print bottom to top 
+
+            for(int i=bottom;i>=top;i--){
+                System.out.print(arr[i][left]);
+            }
+
+            left++;
+
+
+
+
+
+        }
+
+
+
 }
+
+
+  }  

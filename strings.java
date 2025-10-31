bd1efc692928701bc868cc4d55ac5c55d332271a
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-10-31 17:49:28 +0000

    Oct 31, 2025, 11:19 PM

--- strings.java
+++ strings.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class strings {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(system.in);
+        Scanner sc=new Scanner(System.in);
         System.out.println("Enter the String:");
         String str=sc.nextLine();
         System.out.println(checkpalindrome(str));

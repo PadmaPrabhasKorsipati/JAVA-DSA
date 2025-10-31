8214d67ed5ace241e66a20d8ea156212d4fc8d5a
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-10-31 17:46:01 +0000

    Oct 31, 2025, 11:16 PM

--- strings.java
+++ strings.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class strings {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(system.in);
+        System.out.println("Enter the String:");
+        String str=sc.nextLine();
+        System.out.println(checkpalindrome(str));
+
+
+    }
+
+    public static boolean checkpalindrome(String[] args) {
+        int n=str.length();
+        for(int i=0;i<n/2;i++){
+            if(str.charAt(i)!=str.charAt(n-1-i)){
+                return false;
+            }
+            else{
+                return true
+            }
+        }
+        
+    }
     
 }

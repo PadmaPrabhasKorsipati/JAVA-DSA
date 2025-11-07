431432be396def5ba85f00fc7588ef9928055ed9
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-11-07 18:59:14 +0530

    add

--- strings.java
+++ strings.java
@@ -30,3 +30,41 @@ public class strings {
 }
 
 */
+
+
+
+//shortest distance
+
+import java.util.Scanner;
+
+public class strings {
+
+    public static void main(String[] args) {
+        Scanner  sc=new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String str=sc.nextLine();
+        int x=0;
+        int y=0;
+        for(int i=0;i<str.length();i++ ){
+            char current=str.charAt(i);
+            if(current=='N'){
+                y+=1;
+            }
+            else if(current=='S'){
+                y-=1;
+            }
+            else if(current=='E'){
+                x+=1;
+            }
+            else if(current=='W'){
+                x-=1;
+            }
+        }
+
+        double distance=Math.sqrt(y*y-x*x);
+
+        System.out.println("Distance of a point:"+distance);
+
+        sc.close();
+    }
+}

8772064d1bffe43f6a0e6a9193805c2d942992f3
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2025-11-22 23:07:34 +0530

    Auto commit

--- leetcode.java
+++ leetcode.java
@@ -1,3 +1,34 @@
+//Arrays
+//Zer permutation 
+import java.util.Scanner;
+
 public class leetcode {
-    
+
+   
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+
+    int [] nums=new int[5];
+
+    for(int i=0;i<nums.length;i++){
+        nums[i]=sc.nextInt();
+
+    }
+    System.out.println(buildArray(nums));
+
+sc.close();
+
+   }
+
+    public static int[] buildArray(int[] nums) {
+    int[] ans=new int[nums.length];
+       for(int i=0;i<nums.length;i++) {
+        ans[i]=nums[nums[i]];
+
+       }
+
+       return ans;
+    }
+
+
 }

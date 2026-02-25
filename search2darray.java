3090207a1c61543261bda903cedfb36ac215e2f1
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-02-25 22:54:47 +0530

    Auto commit

---
+++ search2darray.java
@@ -0,0 +1,33 @@
+public class search2darray {
+
+    public static void main(String[] args) {
+
+        int[][] matrix={{10,20,30,40},{15,25,35,45},{28,29,37,49},{33,34,39,50}};
+
+        System.out.println(search(matrix, 37));
+        
+    }
+
+    public static int[] search(int[][] matrix,int target){
+        int r=0;
+        int c=matrix.length-1;
+
+        while(r<matrix.length && c>=0){
+            if(matrix[r][c]==target){
+                return new int[] {r,c};
+            }
+
+            if(matrix[r][c]<target){
+                r++;
+            }
+
+            else{
+                c--;
+            }
+        }
+
+        return new int[] {-1,-1};
+
+    }
+    
+}

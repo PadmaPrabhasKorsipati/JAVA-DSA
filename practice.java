a1baedfe32a92fb58a5fdbfe2086192476f6c68d
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-02-25 09:02:40 +0530

    commit

--- practice.java
+++ practice.java
@@ -117,3 +117,34 @@ public class strings {
 }
 */
 
+/* 
+//even or odd
+
+import java.util.Scanner;
+
+public class practice {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number:");
+        int num=sc.nextInt();
+
+        if(num%2==0){
+            System.out.println("The given number is Even.");
+        }
+
+        else{
+            System.out.println("The given number is odd.");
+        }
+
+        sc.close();
+
+
+    }
+}
+
+*/
+
+
+//Fibonacci Series 
+

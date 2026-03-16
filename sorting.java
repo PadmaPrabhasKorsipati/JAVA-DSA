308e476e33c5984455bdfdbffbac3820401b381b
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-03-16 20:34:54 +0530

    auto save

--- sorting.java
+++ sorting.java
@@ -116,49 +116,41 @@ import java.util.Scanner;
 public class sorting {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter no.of Elements:");
-        int n=sc.nextInt();
+        int n = sc.nextInt();
+
         int temp;
-        int[] arr=new int[n];
+        int[] arr = new int[n];
 
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+        System.out.println("Enter elements:");
 
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
         }
 
-        for(int i=1;i<arr.length-2;i++){
-            for(int j=i+1;j>0;j--){
-                if(arr[j]<arr[j-1]){
-                    temp=arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=temp;
+        // Insertion Sort
+        for(int i = 1; i < arr.length; i++){
+            for(int j = i; j > 0; j--){
+                if(arr[j] < arr[j-1]){
+                    temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
                 }
                 else{
                     break;
-
                 }
-
             }
         }
 
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]+" ");
+        System.out.println("Sorted Array:");
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
         }
 
-       sc.close();
-        }
-
-    
-
-
-        
-    }     
-
-
-
-
- 
-        
-    
-
+        sc.close();
+    }
+}

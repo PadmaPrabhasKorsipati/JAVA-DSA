e6647aed95d0e8268e048370f4227d2a16552cdd
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-08-01 17:51:08 +0530

    Auto commit

--- devscops.java
+++ devscops.java
@@ -57,7 +57,7 @@ public class devscops {
         int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
 
         for(int[] num:arr){
-            System.out.println(num);
+            System.out.println(Arrays.toString(num));
         }
 
         

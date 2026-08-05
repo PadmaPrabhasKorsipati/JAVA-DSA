ac4cdc7c75901632c5e6d54e412b98a0c92ae54c
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-08-05 18:58:25 +0530

    Auto commit

--- sorting.java
+++ sorting.java
@@ -4,7 +4,7 @@
 /**
  * sorting
  */
-import java.util.*;
+/* import java.util.*;
 public class sorting {
  public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
@@ -26,15 +26,20 @@ public class sorting {
     static String BubbleSort(int[] arr){
 
         for(int i=0;i<arr.length-1;i++){
-            int temp=0;
+            boolean swapped=false;
             for(int j=0;j<arr.length-1-i;j++){
                 if(arr[j]>arr[j+1]){
-                    temp=arr[j+1];
+                   int  temp=arr[j+1];
                     arr[j+1]=arr[j];
                     arr[j]=temp;
+                    swapped=true;
 
                 }
 
+
+            }
+            if(!swapped){
+                break;
             }
         }
 
@@ -42,4 +47,113 @@ public class sorting {
     }
 
     
+} */
+
+
+//selection sort
+
+/**
+ * sorting
+ */
+/* import java.util.*;
+public class sorting {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+
+        }
+
+        System.out.println(Selectionsort(arr));
+
+        sc.close();
+        
+    }
+
+   static String Selectionsort(int[] arr){
+
+    for(int i=0;i<arr.length-1;i++){
+        int min=i;
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[min]>arr[j]){
+                min=j;
+            }
+
+        }
+
+        int temp=arr[min];
+        arr[min]=arr[i];
+        arr[i]=temp;
+
+    }
+
+    return Arrays.toString(arr);
+
+
+   }
+
+
+    
+} */
+
+
+
+//insertion sort
+
+import java.util.*;
+public class sorting {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+
+
+        }
+
+        System.out.println(InsertionSort(arr));
+
+        sc.close();
+
+    }
+
+    static String InsertionSort(int[] arr){
+
+        for(int i=0;i<arr.length-1;i++){
+
+            for(int j=i+1;j>=0;j++){
+
+                if(arr[j-1]>arr[j]){
+                    int temp=arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=temp;
+                }
+
+                else{
+                    break;
+                }
+
+            }
+
+            
+           
+        }
+
+        return Arrays.toString(arr);
+
+
+    }
+
+
 }

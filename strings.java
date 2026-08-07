89bad3562f7b909e221c3cea0eb8aa23b784fcb1
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-08-07 17:42:32 +0530

    Auto commit

--- strings.java
+++ strings.java
@@ -95,7 +95,8 @@ import java.util.*;
 public class strings {
 
     public static void main(String[] args) {
-      System.out.println(args[0]);
-      System.out.println(args[1]);
+      String name="";
+
+      System.out.println(name);
     }
 }

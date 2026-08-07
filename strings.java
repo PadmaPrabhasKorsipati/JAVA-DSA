8fe7d6cc5ed2d8e743772b643cab057b27ba7aa1
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-08-07 18:10:05 +0530

    Auto commit

--- strings.java
+++ strings.java
@@ -95,9 +95,9 @@ import java.util.*;
 public class strings {
 
     public static void main(String[] args) {
-     float num=12.342323;
+     float num=12.342323f;
 
-     System.out.println(num);
+     System.out.printf("The formated string is:%.2f."+num);
 
     }
 }

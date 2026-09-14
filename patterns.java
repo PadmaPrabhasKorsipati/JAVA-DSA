f003d0d14e06666c19f8b0b771164c6a0ce9275c
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-09-14 18:43:03 +0530

    Auto commit

--- patterns.java
+++ patterns.java
@@ -456,7 +456,7 @@ class HollowSquare {
     }
 } */
 
-
+/* 
 import java.util.Scanner;
 
 class InvertedPyramid {
@@ -472,4 +472,30 @@ class InvertedPyramid {
             System.out.println();
         }
     }
+} */
+
+
+import java.util.Scanner;
+
+class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int rev = 0;
+
+
+        while(n!=0){
+            int remainder=n%10;
+
+            rev=rev*10 +remainder;
+
+            n=n/10;
+
+        }
+
+        System.out.println("Reversed number:"+rev);
+
+}
+
 }

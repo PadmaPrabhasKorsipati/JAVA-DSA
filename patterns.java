c42291ba3fd3a473efdcaf1f7c0cb3ec606547fb
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-09-14 18:01:07 +0530

    Auto commit

--- patterns.java
+++ patterns.java
@@ -424,7 +424,7 @@ class InvertedPyramid {
             for(int j=1;j<=n-i;j++){
                 System.err.print(" ");
             }
-            for(int k=1;k<2*i-1;k++){
+            for(int k=1;k<=2*i-1;k++){
                 System.out.print("*");
             }
 

b778473cd17ef48f52af107354d52c0032f05dc8
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-09-14 20:01:28 +0530

    Auto commit

--- exam.java
+++ exam.java
@@ -631,7 +631,7 @@ import java.util.*;
 /**
  * exam
  */
-public class exam {
+/* public class exam {
 
     public static void main(String[] args) {
 
@@ -671,5 +671,39 @@ public class exam {
             System.out.println("The given number is not a armstrong number");
         }
        
+    }
+} */
+
+
+
+import java.util.*;
+
+public class exam {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+           int count=1;
+
+           if(a[i]==-1){
+            continue;
+           }
+
+           for(int j=i+1;j<n;j++){
+            if(a[i]==a[j]){
+                count++;
+                a[j]=-1;
+            }
+            
+           }
+
+            System.out.println(a[i] + " | " + count);
+        }
     }
 }

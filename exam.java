292685e0fb7964f295a8a84715c059ee3ba4949c
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-09-15 06:58:40 +0530

    Auto commit

--- exam.java
+++ exam.java
@@ -765,11 +765,11 @@ class InvertedPyramid {
         int n = sc.nextInt();
 
         for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= n - i; j++)
-                System.out.print("*");
-
-            for (int j = 1; j <= 2 * i - 1; j++)
+            for (int j = 1; j <= n-1; j++)
                 System.out.print(" ");
+
+            for (int j = 0; j <= n-i; j++)
+                System.out.print("*");
 
             System.out.println();
         }
